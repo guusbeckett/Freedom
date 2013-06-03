2d18c07844433ba5f8d9c3f9ec0c29414f3e08fd
loljimmo12 <spongebob.958@gmail.com>
2013-06-03 15:25:30 +0200

    Added sockets, handle nothing yet

--- src/nl/reupload/freedompainter/ConnectionClient.java
+++ src/nl/reupload/freedompainter/ConnectionClient.java
@@ -1,22 +1,62 @@
 package nl.reupload.freedompainter;
 
 import java.awt.Image;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class ConnectionClient {
-
 	
+	private ServerSocket serverSocket = null;
+	private Socket clientSocket = null;
 	private Paint paint;
+	private ObjectOutputStream out;
+	private ObjectInputStream in;
 
 	public ConnectionClient(Paint paint) {
 		this.paint = paint;
+		try {
+            serverSocket = new ServerSocket(3038);
+            clientSocket = new Socket("localhost", 3038);
+            out = new ObjectOutputStream(clientSocket.getOutputStream());
+            in = new ObjectInputStream(clientSocket.getInputStream());
+        } catch (UnknownHostException e) {
+            System.err.println("Cannot find host, socket init failed");
+            //System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for socket");
+            e.printStackTrace();
+//            System.exit(1);
+        }
 	}
 
 	public void sendImage(Image image) {
-		//TODO stub method
+		try {
+			out.writeObject(image);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public Image getImage() {
 		// TODO stub method
+		try {
+			if (in.available() > 0)
+				return (Image) in.readObject();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 }
--- src/nl/reupload/freedompainter/Main.java
+++ src/nl/reupload/freedompainter/Main.java
@@ -9,6 +9,6 @@ public class Main {
 
 	public static void main(String[] args){
 		paint = new Paint();
-		client = new ConnectionClient();
+		client = new ConnectionClient(paint);
 	}
 }
