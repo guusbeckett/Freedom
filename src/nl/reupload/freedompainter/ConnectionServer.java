db1ca7c6a1d3478bddb1ad5a95f2053d71e6081e
loljimmo12 <spongebob.958@gmail.com>
2013-06-07 16:46:16 +0200

    Fixed UI Freeze

--- src/nl/reupload/freedompainter/ConnectionServer.java
+++ src/nl/reupload/freedompainter/ConnectionServer.java
@@ -3,6 +3,9 @@ package nl.reupload.freedompainter;
 import java.io.IOException;
 import java.net.ServerSocket;
 
+import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
+
 public class ConnectionServer {
 
 	private ServerSocket serverSocket;
@@ -14,20 +17,42 @@ public class ConnectionServer {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		Thread t = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				while (true) {
-					try {
-						new ConnectionHandler(serverSocket.accept());
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+//			try {
+//				new ConnectionHandler(serverSocket.accept());
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+			SwingUtilities.invokeLater(new Runnable() {
+			
+				@Override
+				public void run() {
+					while (true) {
+						try {
+							new ConnectionHandler(serverSocket.accept());
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
 					}
+					
 				}
-				
-			}
-		});
-		t.run();
+			});
+//		Thread t = new Thread(new Runnable() {
+//			
+//			@Override
+//			public void run() {
+//				while (true) {
+//					try {
+//						new ConnectionHandler(serverSocket.accept());
+//					} catch (IOException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//				}
+//				
+//			}
+//		});
+//		t.run();
 	}
 }
